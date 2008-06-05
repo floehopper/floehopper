845e04242e394e236b429fb0f207b256cb1a4384
jamesmead44 <jamesmead44@6b061e51-7646-0410-b801-27bcb2126d9c>
2008-06-05 14:02:58 +0000

    Extracted pre_commit & post_commit methods out of commit script.

--- temp/vendor/plugins/asbo/lib/asbo.rb
+++ temp/vendor/plugins/asbo/lib/asbo.rb
@@ -1,7 +1,11 @@
+require 'rexml/document'
+require 'digest/md5'
+require 'yaml'
+
 module Asbo
   
   class << self
-
+    
     def tests_passed(task_name)
       abort "Warning: No test task name specified" if task_name.empty?
       FileUtils.mkdir_p(File.join('tmp', 'asbo'))
@@ -13,7 +17,6 @@ module Asbo
     def svn_base_revision
       svn_log = `svn log --revision BASE --xml`
       abort "Warning: unable to execute 'svn log'." unless $?.success?
-      require 'rexml/document'
       document = REXML::Document.new(svn_log)
       Integer(document.elements['/log/logentry'].attributes['revision'])
     end
@@ -21,8 +24,30 @@ module Asbo
     def svn_diff_md5
       svn_diff = `svn diff`
       abort "Warning: unable to execute 'svn diff'." unless $?.success?
-      require 'digest/md5'
       Digest::MD5.hexdigest(svn_diff)
+    end
+    
+    def pre_commit
+      current_working_set = { 'svn_base_revision' => Asbo::svn_base_revision, 'svn_diff_md5' => Asbo::svn_diff_md5 }
+      errors = []
+      Dir.glob(File.join('tmp', 'asbo', '*.yml')).each do |filename|
+        working_set = YAML.load(File.open(filename))
+        unless current_working_set['svn_base_revision'] == working_set['svn_base_revision']
+          errors << "svn_base_revisions differ for #{filename}"
+        end
+        unless current_working_set['svn_diff_md5'] == working_set['svn_diff_md5']
+          errors << "svn_diff_md5s differ for #{filename}"
+        end
+      end
+      abort errors.join("\n") unless errors.empty?
+    end
+    
+    def post_commit
+      Dir.glob(File.join('tmp', 'asbo', '*.yml')).each do |filename|
+        working_set = YAML.load(File.open(filename))
+        working_set['svn_diff_md5'] = Asbo::svn_diff_md5
+        File.open(filename, 'w') { |file| file.puts(working_set.to_yaml) }
+      end
     end
     
   end
--- temp/vendor/plugins/asbo/script/commit.rb
+++ temp/vendor/plugins/asbo/script/commit.rb
@@ -1,20 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'yaml'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'asbo')
 
-current_working_set = { 'svn_base_revision' => Asbo::svn_base_revision, 'svn_diff_md5' => Asbo::svn_diff_md5 }
-errors = []
-Dir.glob(File.join('tmp', 'asbo', '*.yml')).each do |filename|
-  working_set = YAML.load(File.open(filename))
-  unless current_working_set['svn_base_revision'] == working_set['svn_base_revision']
-    errors << "svn_base_revisions differ for #{filename}"
-  end
-  unless current_working_set['svn_diff_md5'] == working_set['svn_diff_md5']
-    errors << "svn_diff_md5s differ for #{filename}"
-  end
-end
-abort errors.join("\n") unless errors.empty?
+Asbo::pre_commit
 
 require 'optparse'
 
@@ -33,9 +21,4 @@ command = words.join(" ")
 `#{command}`
 abort "Warning: unable to execute '#{command}'." unless $?.success?
 
-Dir.glob(File.join('tmp', 'asbo', '*.yml')).each do |filename|
-  working_set = YAML.load(File.open(filename))
-  working_set['svn_diff_md5'] = Asbo::svn_diff_md5
-  File.open(filename, 'w') { |file| file.puts(working_set.to_yaml) }
-end
-
+Asbo::post_commit
